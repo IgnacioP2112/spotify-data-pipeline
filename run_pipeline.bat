0c9afc1b8f93abea4834d4bbdc442c679b6d75ea
freebird2112 <ignacio.perezn@sansano.usm.cl>
2026-05-04 19:42:35 -0400

    Remove unnecessary files

--- run_pipeline.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d "C:\Users\Pc\Documents\VS\spotify-data-pipeline"
-call .venv\Scripts\activate
-set PYTHONPATH=C:\Users\Pc\Documents\VS\spotify-data-pipeline
-python pipeline/run_pipeline.py >> logs\pipeline.log 2>&1
